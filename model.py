fb5cb108e73772f2127ddab44071f1f23f7d59c5
lcaflood <lcaflood@gmail.com>
2023-03-10 11:25:41 -0800

    python model file with import

---
+++ model.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+import tensorflow_datasets as tfds
+
+(ds_train, ds_test), ds_info = tfds.load(
+    'mnist',
+    split=['train', 'test'],
+    shuffle_files=True,
+    as_supervised=True,
+    with_info=True,
+)
