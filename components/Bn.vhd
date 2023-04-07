c01ccf792cac46b5b197ef3e4792e4156add7819
jmateo <justin_mateo@sfu.ca>
2023-04-06 21:29:58 -0700

    Created VHDL code structure, and some relu

---
+++ components/Bn.vhd
@@ -0,0 +1,20 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+-- Entity part of the description.  Describes inputs and outputs
+
+entity Bn is
+  port(
+  
+		);
+end Bn;
+
+-- Architecture part of the description
+
+architecture behavioural of Bn is
+
+
+begin
+	
+end behavioural;
---
+++ components/prevariance.vhd
@@ -0,0 +1,20 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+-- Entity part of the description.  Describes inputs and outputs
+
+entity prevariance is
+  port(
+  
+		);
+end prevariance;
+
+-- Architecture part of the description
+
+architecture behavioural of prevariance is
+
+
+begin
+	
+end behavioural;
