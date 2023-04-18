820a59ff8c7d3c633fcea088cde1b88040dee76b
lcaflood <lcaflood@gmail.com>
2023-04-18 14:42:12 -0700

    tested bmxbv

---
+++ git-vhdl/bmxbv_128_10.vhd
@@ -0,0 +1,73 @@
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.STD_LOGIC_UNSIGNED.ALL;
+USE IEEE.NUMERIC_STD.ALL;
+
+ENTITY bmxbv_128_10 IS
+	GENERIC(N 	: NATURAL := 128;
+			  M	: NATURAL := 10); 
+	PORT(CLOCK_50 : IN  STD_LOGIC;
+		START	: IN	STD_LOGIC;
+		vix 		: IN  STD_LOGIC_VECTOR(N-1 downto 0);
+		qwx		: IN	STD_LOGIC_VECTOR(N-1 downto 0);
+		vox		: OUT	INTEGER := 0;
+		DONE		: OUT STD_LOGIC := '0'); 
+END bmxbv_128_10;
+
+ARCHITECTURE RTL OF bmxbv_128_10 IS
+
+	COMPONENT popcnt_128 IS
+	GENERIC(N	: NATURAL := 128); 
+	PORT(INPUT 	: IN  STD_LOGIC_VECTOR(N-1 downto 0);
+		SUM		: OUT INTEGER := 0;
+		DONE		: OUT STD_LOGIC := '0');  
+	END COMPONENT;
+
+	SIGNAL p_DONE : STD_LOGIC := '0';
+	SIGNAL p_INPUT : STD_LOGIC_VECTOR(N-1 downto 0) := (OTHERS => '0');
+	SIGNAL p_SUM : INTEGER := 0; 
+	
+	TYPE state_type IS (s_XNOR, s_popcnt, s_done);
+	SIGNAL state	: state_type;
+	
+BEGIN
+
+	popcnt0 : popcnt_128 PORT MAP(INPUT => p_INPUT, SUM => p_SUM, DONE => p_DONE);
+	
+	PROCESS(START, CLOCK_50)
+		VARIABLE XNOR_RESULT		:	STD_LOGIC_VECTOR(N-1 downto 0) := (OTHERS => '0');
+	BEGIN 
+	
+	IF Rising_Edge(START) THEN
+		DONE <= '0';
+		state <= s_XNOR;
+	
+	ELSIF Rising_Edge(CLOCK_50) THEN
+	
+		CASE state IS
+			WHEN s_XNOR =>			
+				XNOR_RESULT := vix XNOR qwx;
+				p_INPUT <= XNOR_RESULT;
+				state <= s_popcnt;
+				
+			WHEN s_popcnt =>
+				--p_INPUT <= XNOR_RESULT; -- may need to add a clock to let p_INPUT get its value
+				
+				IF(p_DONE = '1') THEN
+					vox <= p_SUM;
+					DONE <= '1';
+					state <= s_done;					
+				ELSE
+					state <= s_popcnt;
+				END IF;
+			
+			WHEN s_done =>	
+				DONE <= '0';
+		
+		END CASE;
+		
+	END IF;
+
+	END PROCESS;
+
+END RTL;
--- git-vhdl/bmxbv_tb.vhd
+++ git-vhdl/bmxbv_tb.vhd
@@ -14,37 +14,47 @@ END bmxbv_tb;
 ARCHITECTURE test OF bmxbv_tb IS	
 
 	TYPE test_case_record IS RECORD
-      vix : STD_LOGIC_VECTOR((N*numLen)-1 downto 0);
-		qwx : STD_LOGIC_VECTOR((N*numLen)-1 downto 0);
+      vix : STD_LOGIC_VECTOR(N-1 downto 0);
+		qwx : STD_LOGIC_VECTOR(N-1 downto 0);
       expected_vox : INTEGER;
    END RECORD;
 
-   TYPE test_case_array_type IS ARRAY (0 to 2) OF test_case_record;
+   TYPE test_case_array_type IS ARRAY (0 to 5) OF test_case_record;
+   SIGNAL zero : STD_LOGIC_VECTOR(N-11 downto 0) := (OTHERS => '0');
+   SIGNAL input1 : STD_LOGIC_VECTOR(N-1 downto 0) := "1010100000" & zero;
+   SIGNAL input2 : STD_LOGIC_VECTOR(N-1 downto 0) := "0001000000" & zero;
+   SIGNAL input3 : STD_LOGIC_VECTOR(N-1 downto 0) := "1100100110" & zero;
+   SIGNAL v1 : STD_LOGIC_VECTOR(N-1 downto 0) := "0010100000" & zero;
+   SIGNAL v2 : STD_LOGIC_VECTOR(N-1 downto 0) := "1101000000" & zero;
+   SIGNAL v3 : STD_LOGIC_VECTOR(N-1 downto 0) := "0100101100" & zero;
     
    signal test_case_array : test_case_array_type := (
-             ((OTHERS => '0'), (OTHERS => '0'), 0), -- change these
-				 ((OTHERS => '1'), (OTHERS => '1'), 128),
-				 (("10101" & (OTHERS => '0')), "001101" & (OTHERS => '0'), -4)
-             ); 
+        ((OTHERS => '0'), (OTHERS => '0'), 128), -- change these
+	((OTHERS => '0'), (OTHERS => '1'), -128),
+	((OTHERS => '1'), (OTHERS => '1'), 128),
+	(input1, v1, 126),
+	(input2, v2, 124),
+	(input3, v3, 122)
+   ); 
 
-	COMPONENT bmxbv_128 IS
+	COMPONENT bmxbv_128_10 IS
 		GENERIC(N 	: NATURAL := 128;
-				  numLen	: NATURAL := 10); 
-		PORT(START	: IN	STD_LOGIC;
-			CLOCK_50 : IN  STD_LOGIC;
-			vix 		: IN  STD_LOGIC_VECTOR((N*numLen)-1 downto 0);
-			qwx		: IN	STD_LOGIC_VECTOR((N*numLen)-1 downto 0);
-			vox		: OUT	STD_LOGIC_VECTOR((N*numLen)-1 downto 0) := (OTHERS => '0');
+				  M	: NATURAL := 10); 
+		PORT(CLOCK_50 : IN  STD_LOGIC;
+			START	: IN	STD_LOGIC;
+			vix 		: IN  STD_LOGIC_VECTOR(N-1 downto 0);
+			qwx		: IN	STD_LOGIC_VECTOR(N-1 downto 0);
+			vox		: OUT	INTEGER := 0;
 			DONE		: OUT STD_LOGIC := '0');  
 	END COMPONENT;
   
 	SIGNAL START, DONE, CLOCK_50 : STD_LOGIC := '0';
-	SIGNAL vix, qwx	:	STD_LOGIC_VECTOR((N*numLen)-1 downto 0) := (OTHERS => '0');
-	SIGNAL vox	:	STD_LOGIC_VECTOR((N*numLen)-1 downto 0)	:= (OTHERS => '0');
+	SIGNAL vix, qwx	:	STD_LOGIC_VECTOR(N-1 downto 0) := (OTHERS => '0');
+	SIGNAL vox	:	INTEGER	:= 0;
 	
 BEGIN
 
-   dut : bmxbv_128 PORT MAP(START => START, CLOCK_50 => CLOCK_50, vix => vix, qwx => qwx, vox => vox, DONE => DONE); 
+   dut : bmxbv_128_10 PORT MAP(START => START, CLOCK_50 => CLOCK_50, vix => vix, qwx => qwx, vox => vox, DONE => DONE); 
  
    PROCESS
 	BEGIN   
@@ -55,20 +65,32 @@ BEGIN
         REPORT "Test case " & integer'image(i) & ":" & " vix = " & integer'image(to_integer(UNSIGNED(test_case_array(i).vix))) & " qwx = " & integer'image(to_integer(UNSIGNED(test_case_array(i).qwx)));
         
         vix <= test_case_array(i).vix;  
-		  qwx <= test_case_array(i).qwx; 		  
-        WAIT FOR 1 ns;
+	qwx <= test_case_array(i).qwx;
+	START <= '1';	
+	WAIT FOR 3 ns;
+	START <= '0';  
+        WAIT UNTIL (DONE = '1');
+	
               
-        REPORT "Expected result: SUM = " & integer'image(test_case_array(i).expected_vox);
-        REPORT "Observed result: SUM = " & integer'image(TO_INTEGER(UNSIGNED(vox)));
+        REPORT "Expected result: vox = " & integer'image(test_case_array(i).expected_vox);
+        REPORT "Observed result: vox = " & integer'image(vox);
                                                                     
-        ASSERT (TO_INTEGER(UNSIGNED(vox)) = test_case_array(i).expected_vox)
+        ASSERT (vox = test_case_array(i).expected_vox)
             REPORT "MISMATCH.  THERE IS A PROBLEM IN YOUR DESIGN THAT YOU NEED TO FIX"
-            SEVERITY warning;
+            SEVERITY warning;	
       END LOOP;
                                            
       REPORT "================== DONE TESTS =============================";
                                                                               
       WAIT; --- we are done.  Wait for ever
     END PROCESS;
+
+clock : PROCESS IS
+BEGIN
+	CLOCK_50 <= '0';
+	WAIT FOR 1 ns;
+	CLOCK_50 <= '1';
+	WAIT FOR 1 ns;
+END PROCESS;
 	
 END test;
