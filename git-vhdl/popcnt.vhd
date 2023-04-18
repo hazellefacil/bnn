ff3f6a7d9a2751a4e858ffd1e4657edab1efd503
lcaflood <lcaflood@gmail.com>
2023-04-17 22:25:00 -0700

    tested popcnt

--- git-vhdl/popcnt.vhd
+++ git-vhdl/popcnt.vhd
@@ -5,38 +5,34 @@ USE IEEE.NUMERIC_STD.ALL;
 
 ENTITY popcnt IS
 	GENERIC(numLen	: NATURAL := 10;
-			  sumLen	: NATURAL := 4); 
-	PORT(START	: IN	STD_LOGIC;  
-		INPUT 	: IN  STD_LOGIC_VECTOR(numLen-1 downto 0);
-		SUM		: OUT UNSIGNED(sumLen-1 downto 0) := (OTHERS => '0');
-		DONE		: OUT STD_LOGIC := '0'); 
+		sumLen	: NATURAL := 4); 
+	PORT(INPUT 	: IN  STD_LOGIC_VECTOR(numLen-1 downto 0);
+		SUM	: OUT UNSIGNED(sumLen-1 downto 0) := (OTHERS => '0');
+		DONE	: OUT STD_LOGIC := '0'); 
 END popcnt;
 
-ARCHITECTURE RTL OF popcnt IS 	 
+ARCHITECTURE RTL OF popcnt IS	 
 	
-BEGIN
-	
-	PROCESS(START)
-		VARIABLE tempSUM	:	UNSIGNED(sumLen-1 downto 0) := (OTHERS => '0');
-		CONSTANT ZERO		:	UNSIGNED(sumLen-1 downto 0) := (OTHERS => '0');
-		CONSTANT	COUNT_MAX	:	INTEGER := numLen;
-	BEGIN 
-		
-		IF Rising_Edge(START) THEN
-			DONE <= '0';
-		
-			FOR COUNT IN 0 TO COUNT_MAX LOOP
+BEGIN	
 
-				tempSUM := tempSUM + (ZERO & INPUT(COUNT)); 
-				
-			END LOOP;
+	PROCESS(INPUT) 
+		VARIABLE tempSUM	:	UNSIGNED(sumLen-1 downto 0) := (OTHERS => '0');
+		CONSTANT ZERO		:	UNSIGNED(sumLen-2 downto 0) := (OTHERS => '0');
+		CONSTANT COUNT_MAX	:	INTEGER := numLen - 1;
+	BEGIN 
+
+		DONE <= '0';
+		tempSUM := (OTHERS => '0');
+		
+		FOR COUNT IN 0 TO COUNT_MAX LOOP
+			tempSUM := tempSUM + (ZERO & INPUT(COUNT)); 
+		END LOOP;
 			
-			tempSUM := tempSUM(sumLen-2 downto 0) & '0'; -- << 1
-			tempSUM := tempSUM - numLen;
+		--tempSUM := tempSUM(sumLen-2 downto 0) & '0'; -- << 1
+		--tempSUM := tempSUM - numLen;
 			
-			SUM <= tempSUM;
-			DONE <= '1';
-		END IF;
+		SUM <= tempSUM;
+		DONE <= '1';
 
 	END PROCESS;
 
--- git-vhdl/popcnt_tb.vhd
+++ git-vhdl/popcnt_tb.vhd
@@ -18,48 +18,50 @@ ARCHITECTURE test OF popcnt_tb IS
       expected_SUM : INTEGER;
    END RECORD;
 
-   TYPE test_case_array_type IS ARRAY (0 to 2) OF test_case_record;
+   TYPE test_case_array_type IS ARRAY (0 to 3) OF test_case_record;
     
    signal test_case_array : test_case_array_type := (
-             ((OTHERS => '0'), 0),
-				 ((OTHERS => '1'), 10),
-				 (("10101" & (OTHERS => '0')), 3)
-             ); 
+        ((OTHERS => '0'), 0),
+	((OTHERS => '1'), 10),
+	(("1010100000"), 3),
+	(("1100100110"), 5)
+    ); 
 
 	COMPONENT popcnt IS
 		GENERIC(numLen	: NATURAL := 10;
 				  sumLen	: NATURAL := 4); 
-		PORT(START	: IN	STD_LOGIC;  
-			INPUT 	: IN  STD_LOGIC_VECTOR(numLen-1 downto 0);
+		PORT(INPUT 	: IN  STD_LOGIC_VECTOR(numLen-1 downto 0);
 			SUM		: OUT UNSIGNED(sumLen-1 downto 0) := (OTHERS => '0');
 			DONE		: OUT STD_LOGIC := '0'); 
 		END COMPONENT;
   
-	SIGNAL START, DONE : STD_LOGIC := '0';
+	SIGNAL DONE : STD_LOGIC := '0';
 	SIGNAL INPUT : STD_LOGIC_VECTOR(numLen-1 downto 0) := (OTHERS => '0');
 	SIGNAL SUM : UNSIGNED(sumLen-1 downto 0) := (OTHERS => '0');
 	
 BEGIN
 
-   dut : popcnt PORT MAP(START => START, INPUT => INPUT, SUM => SUM, DONE => DONE); 
+   dut : popcnt PORT MAP(INPUT => INPUT, SUM => SUM, DONE => DONE); 
  
    PROCESS
 	BEGIN   
+	
       
       FOR i IN test_case_array'LOW TO test_case_array'HIGH LOOP
         
         REPORT "-------------------------------------------";
-        REPORT "Test case " & integer'image(i) & ":" & " INPUT =" & integer'image(to_integer(UNSIGNED(test_case_array(i).INPUT)));
+        REPORT "Test case " & integer'image(i) & ":" & " INPUT = " & integer'image(to_integer(UNSIGNED(test_case_array(i).INPUT)));
         
         INPUT <= test_case_array(i).INPUT;                
         WAIT FOR 1 ns;
               
-        REPORT "Expected result: SUM =" & integer'image(test_case_array(i).expected_SUM);
-        REPORT "Observed result: SUM =" & integer'image(to_integer(SUM));
+        REPORT "Expected result: SUM = " & integer'image(test_case_array(i).expected_SUM);
+        REPORT "Observed result: SUM = " & integer'image(to_integer(SUM));
                                                                     
         ASSERT (to_integer(SUM) = test_case_array(i).expected_SUM)
             REPORT "MISMATCH.  THERE IS A PROBLEM IN YOUR DESIGN THAT YOU NEED TO FIX"
             SEVERITY warning;
+
       END LOOP;
                                            
       REPORT "================== DONE TESTS =============================";
